aa93af8f5cade2a6d82c6c77d47632d3868e80dc
Tomoji Kanae <168693891+Tomoooji@users.noreply.github.com>
2026-06-23 12:01:02 +0900

    add basic setter method

--- src/triangle.h
+++ src/triangle.h
@@ -10,4 +10,15 @@ struct Position{
   void RAtoXY();
 };
 
-class Triangle{};
+class Triangle{
+private:
+  Position _B;
+  Position _C;
+public:
+  Triangle(float lenAB, float lenBC);
+  Position updateCxy(float x, float y, bool as_delta);
+  Position updateCra(float r, float a, bool as_delta);
+  float updateBang(float a, bool as_delta);
+  float updateAang(float a, bool as_delta);
+  bool setBtype(bool at_upper);
+};
